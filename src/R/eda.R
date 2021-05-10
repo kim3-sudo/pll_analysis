710a24b99e1fa39e32bb22e7d0b92930913ebafd
Sejin Kim <kim3@kenyon.edu>
2021-05-10 11:30:04 -0400

    Added barebones R script files for EDA and gboosting

---
+++ src/R/eda.R
@@ -0,0 +1,8 @@
+# PLL Groundball Analysis
+# Sejin Kim
+# STAT 306 Sports Analytics
+
+# This script is for EDA
+
+library(mosaic)
+library(dplyr)
---
+++ src/R/gbr.R
@@ -0,0 +1,9 @@
+# PLL Groundball Analysis
+# Sejin Kim
+# STAT 306 Sports Analytics
+
+# This script is for the gradient boosted regressor
+
+library(mosaic)
+library(dplyr)
+library(gbm)
