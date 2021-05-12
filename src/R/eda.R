26e6faa0ddc9afcaaf397aa4dd2c77b98f1a1d55
Sejin Kim <kim3@kenyon.edu>
2021-05-12 19:45:27 -0400

    Add new packages and build some plots

--- src/R/eda.R
+++ src/R/eda.R
@@ -6,4 +6,48 @@
 
 library(mosaic)
 library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(rstatix)
 
+# Load data
+pll <- readRDS(url('https://github.com/kim3-sudo/pll_analysis/blob/main/data/pll.RDS?raw=true'))
+
+# Determine possible interaction effects
+pllmod <- lm(GB ~ Pos * GP * P * G1 * G2 * A * Sh * SOG * TO * CT * Team, data = pll)
+interaction.plot(pll$GP, pll$Pos, pll$GB) # YES
+interaction.plot(pll$P, pll$Pos, pll$GB) # YES
+interaction.plot(pll$G1, pll$Pos, pll$GB) # NO
+interaction.plot(pll$G2, pll$Pos, pll$GB) # NO
+interaction.plot(pll$A, pll$Pos, pll$GB) # YES
+interaction.plot(pll$Sh, pll$Pos, pll$GB) # WTH
+interaction.plot(pll$SOG, pll$Pos, pll$GB) # Also WTH
+
+# Use Friedman test to determine interaction (a nonpar alternative to two-way ANOVA)
+# Visualize using a boxplot for sanity
+ggboxplot(pll, x = "Pos", y = "GB", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "GP", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "P", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "G1", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "G2", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "A", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "Sh", add = "jitter")
+ggboxplot(pll, x = "Pos", y = "SOG", add = "jitter")
+ggboxplot(pll, x = "GP", "P", add = "jitter")
+ggboxplot(pll, x = "GP", "G1", add = "jitter")
+ggboxplot(pll, x = "GP", "G2", add = "jitter")
+ggboxplot(pll, x = "GP", "A", add = "jitter")
+ggboxplot(pll, x = "GP", "Sh", add = "jitter")
+ggboxplot(pll, x = "GP", "SOG", add = "jitter")
+ggboxplot(pll, x = "G1", "G2", add = "jitter")
+ggboxplot(pll, x = "G1", "A", add = "jitter")
+ggboxplot(pll, x = "G1", "Sh", add = "jitter")
+ggboxplot(pll, x = "G1", "SOG", add = "jitter")
+ggboxplot(pll, x = "G2", "A", add = "jitter")
+ggboxplot(pll, x = "G2", "Sh", add = "jitter")
+ggboxplot(pll, x = "G2", "SOG", add = "jitter")
+ggboxplot(pll, x = "A", "Sh", add = "jitter")
+ggboxplot(pll, x = "A", "SOG", add = "jitter")
+ggboxplot(pll, x = "Sh", "SOG", add = "jitter")
+# Carry out Friedman tests to determine all significant effects to continue with
+friedman.test(GB ~ Sh | GP, data = as.matrix(pll))
--- src/R/gbr.R
+++ src/R/gbr.R
@@ -8,7 +8,54 @@ library(mosaic)
 library(dplyr)
 library(gbm)
 library(parallel)
+library(leaps)
+library(caret)
+library(MASS)
 
+# Read in data
+pll <- readRDS(url('https://github.com/kim3-sudo/pll_analysis/blob/main/data/pll.RDS?raw=true'))
+
+# Prepare multicore processing
 numCores <- detectCores()
+print(paste('Will use ', numCores, ' cores in GBM'))
 
-gbm(formula = (GB~Rank*GP*P*Sh), distribution = "poisson", data = pll, n.trees = 100, interaction.depth = 1, cv.folds = 1, verbose = TRUE, n.cores = numCores)
+# Generate models to use with GBM
+## Start with full model using reasonable data
+fullmod <- lm(GB ~ Pos + GP + P + G1 + G2 + A + Sh + SOG + TO + CT + Team, data = pll)
+## Generate stepwise model
+stepmod <- stepAIC(fullmod, direction = "both", trace = FALSE)
+summary(stepmod)
+## Generate max subsetted model using leaps on sequential replacement
+seqrepmod <- regsubsets(GB ~ Pos + GP + P + G1 + G2 + A + Sh + SOG + TO + CT + Team, data = pll, nvmax = 10, method = "seqrep")
+summary(seqrepmod)
+## Generate model via k-fold x-validation
+trainCtrl <- trainControl(method = "cv", number = 10)
+newstepmod <- train(GB ~ Pos + GP + P + G1 + G2 + A + Sh + SOG + TO + CT + Team, data = pll,
+                 method = "leapSeq",
+                 tuneGrid = data.frame(nvmax = 1:10),
+                 trControl = trainCtrl)
+backmod <- train(GB ~ Pos + GP + P + G1 + G2 + A + Sh + SOG + TO + CT + Team, data = pll,
+                 method = "leapBackward",
+                 tuneGrid = data.frame(nvmax = 1:10),
+                 trControl = trainCtrl)
+forwmod <- train(GB ~ Pos + GP + P + G1 + G2 + A + Sh + SOG + TO + CT + Team, data = pll,
+                 method = "leapForward",
+                 tuneGrid = data.frame(nvmax = 1:10),
+                 trControl = trainCtrl)
+summary(newstepmod)
+summary(backmod)
+summary(forwmod)
+
+## View optimal nvmax value - finds the model with the lowest RMSE
+newstepmod$bestTune
+backmod$bestTune
+forwmod$bestTune
+
+## View best models
+summary(newstepmod$finalModel)
+summary(backmod$finalModel)
+summary(forwmod$finalModel)
+
+# Generate gradient boosted modifier model
+gbmmod <- gbm(formula = (GB~Rank*GP*P*Sh), distribution = "poisson", data = pll, n.trees = 100, interaction.depth = 1, cv.folds = 1, verbose = TRUE, n.cores = numCores)
+mod
